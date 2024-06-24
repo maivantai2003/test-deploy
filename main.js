c55500aa7ceb94b2ff65ed5e2221127ca1561d6b
unknown <vantai08122003@gmail.com>
2024-06-25 00:31:42 +0700

    update

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $(document).on("click","#clickMe",function(){
         $(this).text("Hello you")
     })
+    $(document).on("click","#img",function(){
+        alert("Hello: "+$("#name").val())
+        $("#name").val("")
+    })
 })
